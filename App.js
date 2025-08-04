b9c03f84680188be0700e5a31b7dd2031aa5281f
FKode Solution <divyabarathisivanantham1526@gmail.com>
2025-08-04 18:20:51 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,23 @@
-import { Link, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Services from './Services';
-import Contact from './Contact';
-import SearchOrder from './SearchOrder';
-import Offers from './Offers';
-import Login from './Login';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
- 
   return (
-
-
-    
     <div className="App">
-      
-      <nav className='navbar'>
-        <div className='navbar-container'>
-        <ul className='nav-links'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/services">Collections</Link></li>
-          <li><Link to="/Offers">Offers</Link></li>
-     
-              <li><Link to="/SearchOrder">Order</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li className='log'><Link to="/Login">Login/Signup</Link></li>
-        </ul>
-        </div>
-      </nav>
-
-     
-    
-        
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/Offers" element={<Offers />} />
-        <Route path="/contact" element={<Contact />} />
-     
-         <Route path="/SearchOrder" element={<SearchOrder />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-
-
-      
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
